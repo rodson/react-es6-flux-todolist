5228c99f10aa28737c239a24cdbd985e2f6cc7ad
rodson <rodsonluo@gmail.com>
2015-10-08 23:13:48 +0800

    add gulp task

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+
+gulp.task('build', function() {
+  return browserify({
+      entries: './js/app.js',
+      debug: true
+    })
+    .transform(babelify)
+    .bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['build']);
